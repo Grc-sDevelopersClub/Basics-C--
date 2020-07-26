1dec2bbe6ac3594d893a5c07a2e274911ae0c0a7
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-26 18:51:38 +0530

    preprocessors and basic declaration

---
+++ Preprocessor.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+
+
+using namespace std;
+int fun()
+{
+    int a=10,b=20;
+    // int sum;
+    
+    // 
+    return a+b*a/b+b;
+
+}
+
+int main()
+{
+    int sum;
+    int a,b;
+    string ans;
+    // cout<<b<<endl;
+    // b=a;
+    // cout<<b<<endl;
+    cout<<"Enter value of a:";
+    cin>>a;
+    cout<<"Enter value of b:";
+    cin>>b;
+    if(a==b)
+    {
+        ans="True";
+    }
+    else if(a<b)
+    {
+        ans="smaller";
+    }
+    // else
+    // {
+        
+    //     sum=a+b;
+    //     ans=false;
+    // }
+    cout<<ans<<endl;
+    cout<<sum<<endl;
+    return 0;   
+}
+
+
+// iostream-> istream -input stream 
+//         -> ostream -output stream
+
+// int -integer  eg 1,56,200,4000000;
+// float -floating points eg: 0.1, 0.5, 35.4;
+// char-charecters a, b , c ,"1","3","5";
+
