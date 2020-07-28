3131d6e9f6d893663ab127cf9daec049b7d3d53d
chavangorakh1999 <chavangorakh19999@gmail.com>
2020-07-28 20:41:54 +0530

    summary of for loops

--- Preprocessor.cpp
+++ Preprocessor.cpp
@@ -14,32 +14,43 @@ int fun()
 
 int main()
 {
-    int sum;
-    int a,b;
-    string ans;
-    // cout<<b<<endl;
-    // b=a;
-    // cout<<b<<endl;
-    cout<<"Enter value of a:";
-    cin>>a;
-    cout<<"Enter value of b:";
-    cin>>b;
-    if(a==b)
+    int i;
+    int end_limit=5;
+    while(end_limit!=0)
     {
-        ans="True";
+        cout<<"Hello world"<<endl;
+        end_limit --;
     }
-    else if(a<b)
-    {
-        ans="smaller";
-    }
+    // for(i=0;i<end_limit;i++)
+    // {
+    //     cout<<"hello World"<<endl;
+    // }
+    // int sum;
+    // int a,b;
+    // string ans;
+    // // cout<<b<<endl;
+    // // b=a;
+    // // cout<<b<<endl;
+    // cout<<"Enter value of a:";
+    // cin>>a;
+    // cout<<"Enter value of b:";
+    // cin>>b;
+    // if(a==b)
+    // {
+    //     ans="True";
+    // }
+    // else if(a>b)
+    // {
+    //     ans="smaller";
+    // }
     // else
     // {
         
     //     sum=a+b;
     //     ans=false;
     // }
-    cout<<ans<<endl;
-    cout<<sum<<endl;
+    // cout<<ans<<endl;
+    // cout<<sum<<endl;
     return 0;   
 }
 
@@ -48,6 +59,6 @@ int main()
 //         -> ostream -output stream
 
 // int -integer  eg 1,56,200,4000000;
-// float -floating points eg: 0.1, 0.5, 35.4;
+// float - floating points eg: 0.1, 0.5, 35.4;
 // char-charecters a, b , c ,"1","3","5";
 
